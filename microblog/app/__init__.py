1e29fc9e7c586b089b39c86fcbf288070bedd515
Blink17072002 <adeyemojoshua17@gmail.com>
2026-04-02 16:49:10 +0100

    chapter 5

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from .config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 
 app = Flask(__name__)
@@ -10,5 +11,8 @@ app.config.from_object(Config)
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+login = LoginManager(app)
+login.login_view = 'login' # lets flask_login know which view function handles logins
 
 from app import routes, models
