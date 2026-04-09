1cbf5b2d80aa30ba8ce2d3374a7bc4733cd4eadd
Blink17072002 <adeyemojoshua17@gmail.com>
2026-04-09 17:37:44 +0100

    chapter 7

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -3,11 +3,14 @@ from .config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+import logging
+from logging.handlers import SMTPHandler, RotatingFileHandler
+import os
 
 
 app = Flask(__name__)
 
-app.config.from_object(Config)
+app.config.from_object(Config)  # read and apply the config file
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
@@ -15,4 +18,35 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login' # lets flask_login know which view function handles logins
 
-from app import routes, models
+
+if not app.debug: # email logger when app not in debug mode
+    if app.config['MAIL_SERVER']:
+        auth = None
+        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+            secure = None
+            if app.config['MAIL_USE_TLS']:
+                secure = ()
+            mail_handler = SMTPHandler(
+                mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+                fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+                toaddrs=app.config['ADMINS'], 
+                subject='Microblog Failure',
+                credentials=auth,
+                secure=secure
+            )
+            mail_handler.setLevel(logging.ERROR)
+            app.logger.addHandler(mail_handler)
+
+    if not os.path.exists('logs'):  # create a directory called logs (for the purpose of logging errors and information for debugging)
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240, backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'
+    ))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+    app.logger.info('Microblog startup')
+
+
+from app import routes, models, errors
